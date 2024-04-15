7b3d9d7d1151841e4352bbb5b3cf42d370e0cd23
Aakash Jain <aakashjain245@gmail.com>
2024-04-15 18:39:59 +0530

    worked on the chat section.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,164 @@
+import { useState ,useEffect } from "react"
+import { CiSearch } from "react-icons/ci";
+import {auth , db}  from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { BsSendFill } from "react-icons/bs";
+import { LuPaperclip } from "react-icons/lu";
+
+import {
+    collection,
+    addDoc,
+    where,
+    serverTimestamp,
+    onSnapshot,
+    query,
+    orderBy,
+  } from "firebase/firestore";
+  
+// import { IoIosSearch } from "react-icons/io";
+
 
 function Chat() {
+    const [search,setSearch] = useState("");
+    // const [messages, setMessages] = useState([]);
+    // const [newMessage , setNewMessage] = useState("");
+    // const messagesRef = collection(db,"messages");
+    const [user] = useAuthState(auth);
+    const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState("");
+
+    useEffect(() => {
+      const q = query(collection(db, "messages"), orderBy("timestamp"));
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+  
+      return () => unsubscribe();
+    }, []);
+
+    const handleInputChange = (e) => {
+      setInput(e.target.value);
+    };
+  
+    const sendMessage = async (e) => {
+      e.preventDefault();
+  
+      if (input.trim()) {
+        await addDoc(collection(db, "messages"), {
+          text: input,
+          timestamp: new Date(),
+          uid: user.uid,
+          displayName: user.displayName,
+          photo : user.photoURL
+    });
+  
+    setInput("");
+  } 
+} 
+    // useEffect(() => {
+    //     const queryMessages = query(
+    //       messagesRef,
+    //       orderBy("createdAt")
+    //     );
+    //     const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
+    //       let messages = [];
+    //       snapshot.forEach((doc) => {
+    //         messages.push({ ...doc.data(), id: doc.id });
+    //       });
+    //       console.log(messages);
+    //       setMessages(messages);
+    //     });
+    
+    //     return () => unsuscribe();
+    //   }, []);
+    
+    //   const handleSubmit = async (event) => {
+    //     event.preventDefault();
+    //     if (newMessage.trim() === ""|| !auth.currentUser) return;
+
+    //     const [uid,photoURL, displayName] = auth.currentUser;
+    //     try {
+    //         await addDoc(messagesRef, {
+    //           text: newMessage,
+    //           createdAt: serverTimestamp(),
+    //           user: displayName,
+    //           uid,
+    //           photoURL,
+    //         });
+      
+    //         console.log("Message added to Firestore:", newMessage);
+    //         setNewMessage("");
+    //       } catch (error) {
+    //         console.error("Error adding message to Firestore:", error);
+    //       }
+    //     };
+      
+    //     console.log("Current messages:", messages);
+      
+    
+
   return (
-    <div className='bg-white h-full rounded-2xl' >Chat</div>
+    <div className='bg-white h-full rounded-2xl grid grid-cols-9 p-4' >
+        <div className="col-span-3">
+            <div className="flex gap-4 items-center border-1  p-1.5 rounded-lg w-[70%] bg-purple-200">
+                <CiSearch size={20} color="black"/>
+                <input className="border-none outline-none bg-purple-200" type="text" placeholder="Search" onChange={(e)=>setSearch(e.target.value)}/>
+            </div>
+        </div>
+        <div className=" flex flex-col col-span-6">
+            <div className="ml-8 text-3xl font-semibold mb-5 text-gray-700">Group Chat</div>
+            <div className=" flex-grow overflow-y-auto mb-2" style={{ maxHeight: "calc(100vh - 170px)" }}>
+              {messages.map(({ id, data }) => (
+                <div key={id} className={`message ${data.uid === user.uid ? "sent" : "received"}`}>
+                  <div className="flex items-center mb-3">
+                  <img src={data.photo} alt="photoURL" className="rounded-full w-8 h-8 mr-2" ></img>
+                  {/* <span className="displayName">{data.displayName}: </span> */}
+                  <div className="p-1.5 pr-4 pl-2 border-1 bg-blue-200 rounded-md ">
+                  <span className="messageText">{data.text}</span>
+                  </div>
+                  </div>
+                </div>
+              ))}
+            </div >
+            <div className="flex justify-center items-center gap-2">
+              {/* <LuPaperclip size={24}/> */}
+            <form onSubmit={sendMessage} className="flex justify-center items-center border bg-purple-200 border-gray-300 rounded-md w-[95vh] p-1">
+              <div className="p-2.5 bg-blue-400 rounded-2xl hover:bg-blue-600 hover:cursor-pointer">
+                <LuPaperclip size={16} color="white"/>
+              </div>
+              <input
+                type="text"
+                value={input}
+                onChange={handleInputChange}
+                className="outline-none flex-grow bg-purple-200 ml-2"
+                placeholder="Type your message here..."
+              />
+              <button type="submit" className=" p-2.5 bg-blue-400 text-white rounded-2xl hover:bg-blue-600">
+                <BsSendFill/>
+              </button>
+            </form>
+            </div>
+        </div>
+    </div>
   )
 }
+
+// function ChatMessage(props) {
+//     const { text, uid, photoURL } = props.message;
+  
+//     const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  
+//     return (<>
+//       <div className={`message ${messageClass}`}>
+//         <img src={photoURL} />
+//         <p>{text}</p>
+//       </div>
+//     </>)
+//   }
 
 export default Chat
--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -5,29 +5,37 @@ import { CgProfile } from "react-icons/cg";
 import { FaUserFriends } from "react-icons/fa";
 import { IoMdArchive } from "react-icons/io";
 import { MdGroups } from "react-icons/md";
+import { useState } from "react";
 
 function LeftSection() {
+
+
   return (
     <div className="flex flex-col items-center mt-3 gap-12">
         <RiWechatChannelsFill color="white" size={50} className="mb-7 mt-2" />
         <div>
-            <LeftSectionOptions name="all chats" icon={<IoChatbox onMouseOver={({target})=>target.style.color="white"} onMouseOut={({target})=>target.style.color="gray"} size={25}/>} />
-            <LeftSectionOptions name="groups" icon={<MdGroups onMouseOver={({target})=>target.style.color="white"} onMouseOut={({target})=>target.style.color="gray"} size={25}/>} />
-            <LeftSectionOptions name="friends" icon={<FaUserFriends onMouseOver={({target})=>target.style.color="white"} onMouseOut={({target})=>target.style.color="gray"} size={25}/>} />
-            <LeftSectionOptions name="archive" icon={<IoMdArchive onMouseOver={({target})=>target.style.color="white"} onMouseOut={({target})=>target.style.color="gray"} size={25}/>} />
+            <LeftSectionOptions name="all chats" icon={<IoChatbox  size={25}/>} />
+            {/* <div className="text-white">
+
+            <IoChatbox  size={25}/>
+            </div> */}
+            <LeftSectionOptions name="groups" icon={<MdGroups size={25}/>} />
+            <LeftSectionOptions name="friends" icon={<FaUserFriends size={25}/>} />
+            <LeftSectionOptions name="archive" icon={<IoMdArchive size={25}/>} />
             <div className="h-0.5 bg-gray-400 mb-7"></div>
-            <LeftSectionOptions name="profile" icon={<CgProfile onMouseOver={({target})=>target.style.color="white"} onMouseOut={({target})=>target.style.color="gray"} size={25}/>} />
+            <LeftSectionOptions name="profile" icon={<CgProfile size={25}/>} />
         </div>
         <div>
-            <LeftSectionOptions name="logout" icon={<MdOutlineLogout onMouseOver={({target})=>target.style.color="white"} onMouseOut={({target})=>target.style.color="gray"} size={25}/>} />
+            <LeftSectionOptions name="logout" icon={<MdOutlineLogout size={25}/>} />
         </div>
     </div>
   )
 }
 
 const LeftSectionOptions =(props)=>{
+    const [ hover, setHover ] = useState(false);
     return(
-        <div className="flex flex-col items-center gap-1 text-sm text-gray-400 mb-7 hover:cursor-pointer hover:text-white">
+        <div className={`flex flex-col items-center gap-1 text-sm text-gray-400 mb-7 hover:cursor-pointer ${hover ? 'text-white' : 'text-gray-500'}`} onMouseOver={()=>setHover(true)} onMouseOut={()=>setHover(false)}>
             {props.icon}
             {props.name}
         </div>
